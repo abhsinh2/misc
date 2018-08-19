2bc0b609b278c30757e480cb372b483982ed1969
Abhishek Sinha <abhishek.sinha04@yahoo.in>
2018-08-19 18:50:44 +0530

    Navigating from root

--- json-reader/src/main/java/com/abhsinh2/json/JsonReaderToCSV.java
+++ json-reader/src/main/java/com/abhsinh2/json/JsonReaderToCSV.java
@@ -8,7 +8,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
@@ -17,8 +16,6 @@ import java.util.Map;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
-import org.apache.commons.csv.CSVRecord;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -29,45 +26,90 @@ public class JsonReaderToCSV {
 	public static void main(String[] args) throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode rootNode = mapper.readTree(new File(
-				"/Users/abhsinh2/Abhishek/Study/workspace-sts-3.9.0.RELEASE/json-reader/src/main/resources/test.json"));
+				"/Users/abhsinh2/Abhishek/github/misc/json-reader/src/main/resources/test.json"));
 
-		List<Map<String, String>> records = new ArrayList<>();
-
-		readResult(rootNode.get("d").get("results"), null, records, null);
+		List<Map<String, String>> records = readRoot(rootNode);
 
 		System.out.println("================================");
 		records.forEach(record -> System.out.println(record));
-		
+
 		// Write to csv
 		BufferedWriter writer = Files.newBufferedWriter(Paths.get(
-				"/Users/abhsinh2/Abhishek/Study/workspace-sts-3.9.0.RELEASE/json-reader/src/main/resources/test.csv"));
+				"/Users/abhsinh2/Abhishek/github/misc/json-reader/src/main/resources/test.csv"));
 		CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
-		
-		Collection<String> headers = new LinkedHashSet<>();		
+
+		Collection<String> headers = new LinkedHashSet<>();
 		records.forEach(record -> {
 			headers.addAll(record.keySet());
 		});
-		
+
 		System.out.println("================================");
 		System.out.println(headers);
 		csvPrinter.printRecord(headers);
-		
+
 		System.out.println("================================");
 		records.forEach(record -> {
 			List<String> values = new ArrayList<>();
 			headers.forEach(header -> {
 				values.add(record.get(header));
-			});		
+			});
 			System.out.println(values);
 			try {
 				csvPrinter.printRecord(values);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		});		
+		});
 
 		csvPrinter.flush();
 		csvPrinter.close();
+	}
+
+	private static List<Map<String, String>> readRoot(JsonNode root) {
+		List<Map<String, String>> records = new ArrayList<>();
+		if (root.isObject()) {
+			readObject(root, null, records, null);
+		} else if (root.isArray()) {
+			readArray(root, null, records, null);
+		}
+		return records;
+	}
+
+	private static void readObject(JsonNode node, String parentKey, List<Map<String, String>> records,
+			Map<String, String> record) {
+		Iterator<Map.Entry<String, JsonNode>> childNodeIter = node.fields();
+		while (childNodeIter.hasNext()) {
+			Map.Entry<String, JsonNode> childEntry = childNodeIter.next();
+			String childKey = childEntry.getKey();
+			JsonNode childNode = childEntry.getValue();
+
+			if (childKey.equals("results")) {
+				if (parentKey == null) {
+					readResult(childNode, null, records, record);
+				} else {
+					readResult(childNode, parentKey + "_" + childKey, records, record);
+				}				
+			} else {
+				if (node.isObject()) {
+					readObject(childNode, parentKey, records, record);
+				} else if (node.isArray()) {
+					readArray(childNode, parentKey, records, record);
+				}
+			}
+		}
+	}
+
+	private static void readArray(JsonNode node, String parentKey, List<Map<String, String>> records,
+			Map<String, String> record) {
+		Iterator<JsonNode> nodeIter = node.iterator();
+		while (nodeIter.hasNext()) {
+			JsonNode childNode = nodeIter.next();
+			if (childNode.isObject()) {
+				readObject(childNode, parentKey, records, record);
+			} else if (childNode.isArray()) {
+				readArray(childNode, parentKey, records, record);
+			}
+		}
 	}
 
 	private static void readResult(JsonNode resultNode, String parentKey, List<Map<String, String>> records,
@@ -97,7 +139,11 @@ public class JsonReaderToCSV {
 
 						if (!IGNORE_LIST.contains(key)) {
 							if (value.isArray()) {
-								readArray(value, result);
+								if (parentKey == null) {
+									readArray(value, key, records, newRecord);
+								} else {
+									readArray(value, parentKey + "_" + key, records, newRecord);
+								}
 							} else if (value.isObject()) {
 								if (parentKey == null) {
 									readObject(value, key, records, newRecord);
@@ -122,21 +168,4 @@ public class JsonReaderToCSV {
 		}
 	}
 
-	private static void readObject(JsonNode obj, String parentKey, List<Map<String, String>> records,
-			Map<String, String> record) {
-		Iterator<Map.Entry<String, JsonNode>> entries = obj.fields();
-		while (entries.hasNext()) {
-			Map.Entry<String, JsonNode> entry = entries.next();
-			String key = entry.getKey();
-			JsonNode value = entry.getValue();
-
-			if (key.equals("results")) {
-				readResult(value, parentKey + "_" + key, records, record);
-			}
-		}
-	}
-
-	private static void readArray(JsonNode obj, JsonNode parent) {
-
-	}
 }
